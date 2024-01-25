4fa1fccb7a41d6ddbdc51adaf097ec935e0d4f3b
Stephen Simpson <test@test.com>
2024-01-24 19:12:29 -0600

    Bump

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,22 @@
 // Step 1: Import React
 import * as React from 'react'
+import Layout from '../components/layout'
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
-    <main>
-      <h1>About Me</h1>
-      <p>Hi there! This is the about page that I am testing with Gatsby!</p>
-    </main>
+    <Layout pageTitle="About Me">
+      <p>Hi there, this is my about me page</p>
+    </Layout>
   )
 }
+
+export const Head = () => (
+  <>
+    <title>About Me</title>
+    <meta name="description" content="Your description" />
+  </>
+)
 
 // Step 3: Export your component
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,15 @@
 // Step 1: Import React
 import * as React from 'react'
+import Layout from '../components/layout'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial!!</p>
-    </main>
+    <Layout pageTitle="Home Page!">
+      <p>This is a Gatsby Tutorial</p>
+    </Layout>
   )
 }
-
 // You'll learn about this in the next task, just copy it for now
 export const Head = () => <title>Home Page</title>
 
