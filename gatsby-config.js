2598f4cd2c2374c1943091f3e3fbfcc6e091c674
Stephen Simpson <test@test.com>
2024-01-25 14:35:17 -0600

    Added a duck

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    title: "The Great Gatsby Tutorial"
   },
   plugins: [
     "gatsby-plugin-image",
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,24 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import Seo from '../components/seo'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page!">
       <p>This is a Gatsby Tutorial</p>
+      <StaticImage
+        alt="This is a duck"
+        src="../images/mallard_duck.jpg"
+      />
     </Layout>
   )
 }
 // You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
+// export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title="Home" />
 
 // Step 3: Export your component
 export default IndexPage
